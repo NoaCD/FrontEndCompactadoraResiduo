1aa9fa9bf435a5e70686b4dac710ce7c1624b6a3
noe.c@giotex.com.mx <noe.c@giotex.com.mx>
2022-04-14 18:13:57 -0500

    HomeDashboard

--- FrontEndCompactadoraResiduos/Controllers/HomeController.cs
+++ FrontEndCompactadoraResiduos/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using FrontEndCompactadoraResiduos.Models;
+﻿using FrontEndCompactadoraResiduos.Bussiness.Home;
+using FrontEndCompactadoraResiduos.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -6,16 +7,51 @@ namespace FrontEndCompactadoraResiduos.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<HomeController> _logger;
+        private readonly HomeBussiness homeBus = new HomeBussiness();
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var host = _configuration.GetValue<string>("HostAPI"); //Host del api localhost:8080 | 127.0.0.1:8080
+
+            var totalResiduos = homeBus.getTotalResiduos(host);
+            var totalCargas = homeBus.getTotalCargas(host);
+            var totalUsuarios = homeBus.getTotalUsuarios(host);
+            var modelo = new HomeViewModel(); 
+            if(totalResiduos.Result.estatus == "success" && totalResiduos.Result.codigo == 200 )
+            {
+               modelo = new HomeViewModel()
+               {
+                   totalCargas = totalCargas.Result.data,
+                   totalUsuarios = totalUsuarios.Result.data,
+                   totalResiduos = totalResiduos.Result.data,
+                   mensaje = totalResiduos.Result.mensaje,
+                   estatus = totalResiduos.Result.estatus
+
+               };
+            }
+            else
+            {
+                modelo = new HomeViewModel()
+                {
+                    totalCargas = totalCargas.Result.data,
+                    totalUsuarios = totalUsuarios.Result.data,
+                    totalResiduos = totalResiduos.Result.data,
+                    mensaje = totalResiduos.Result.mensaje,
+                    estatus = totalResiduos.Result.estatus
+
+                };
+            }
+
+            return View(modelo);
         }
 
         public IActionResult Privacy()
---
+++ FrontEndCompactadoraResiduos/Scripts/HomeController/Home.js
@@ -0,0 +1,57 @@
+﻿
+
+/*****************************************
+ *Js de la pagina principa de HOME par cargar el dashoboard
+ * 
+ * 
+ * ********************************************/
+
+$(document).ready(function () {
+
+    init(); //funcion para iniciar le programa
+ 
+
+});
+
+
+function init() {
+
+    var mensaje = $("#mensaje").val();
+    var estatus = $("#estatus").val();
+    if (estatus == "error") {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'error',
+            title: mensaje
+        })
+
+    } else {
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
+
+        Toast.fire({
+            icon: 'success',
+            title: mensaje
+        })
+    }
+}
